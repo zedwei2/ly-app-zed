ba28e45ece66921b8c9e21ae696c96bce26ae900
TENCENT\v_bwweiwang <1598489389@qq.com>
2023-10-09 16:09:17 +0800

    chore: 新增vant组件库

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import 'uno.css';
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import { createSSRApp } from 'vue';
 import App from './App.vue';
 import store from '@/store';
 export function createApp() {
   const app = createSSRApp(App).use(store);
+  app.use(Vant)
   return {
     app
   };
--- src/pages/test/chart.vue
+++ src/pages/test/chart.vue
@@ -5,6 +5,8 @@ import DemoPage from '@/components/demo/index.vue'
 <template>
   <view class="container">
     <DemoPage />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="default">主要按钮</van-button>
   </view>
 </template>
 
