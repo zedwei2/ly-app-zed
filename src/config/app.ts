2b7013f1cef74c4a0698f1b3041065236b3f6551
TENCENT\v_bwweiwang <1598489389@qq.com>
2023-10-10 14:57:26 +0800

    refactor: remove project

--- src/config/app.ts
+++
@@ -1,2 +0,0 @@
-export const APP_ID = 'xxxxx';
-export const APP_VERSION = '1.0.0';
--- src/store/index.ts
+++
@@ -1,6 +0,0 @@
-import piniaPluginPersist from 'pinia-plugin-persist-uni';
-
-const store = createPinia();
-store.use(piniaPluginPersist);
-
-export default store;
