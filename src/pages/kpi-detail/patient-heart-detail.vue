062fe258f76cbbfbe93750d183b920eb22123aa6
v_bwweiwang <v_bwweiwang@tencent.com>
2023-10-11 16:04:34 +0800

    feat: 完成心率图

--- src/pages/kpi-detail/patient-heart-detail.vue
+++ src/pages/kpi-detail/patient-heart-detail.vue
@@ -1,106 +1,78 @@
 <template>
   <view class="hr-detail">
     <dateBar />
-    <view class="hr-chart">
-      <view class="hr-chart-title"><text>65</text>次/分钟</view>
-      <qiun-data-charts 
-        type="area"
-        :opts="chart.opts"
-        :chartData="chart.chartData"
-      />
+    <areaChart />
+    <view class="hr-card">
+      <view  
+        class="hr-card-item"
+        v-for="item in hrCardData"
+        :key="item.title">
+        <view class="title">{{ item.title }}</view>
+        <view class="value"><text>{{ item.value }}</text>次/分钟</view>
+      </view>
     </view>
   </view>
 </template>
 
 <script lang="ts" setup>
 import dateBar from "./component/date-bar.vue";
-
-const getData = () => {
-  var arr: any = []
-  var q = 1597910400000
-  for(var i=1; i<100; i++){
-    arr.push([q-(i*3600*1000), (Math.random()*10)+80])
-    q = q-(i*3600*1000)
-  }
-  return arr
-}
-console.log(getData())
-const chart =  {
-  chartData: {
-    series: [
-      {
-       
-        data: getData()
-      }
-    ]
+import areaChart from "./component/area-chart.vue";
+const hrCardData = [
+  {
+    title: '心率范围',
+    value: '60-103'
   },
-  opts: {
-    color: ["#FF5D63","#91CB74","#FAC858","#EE6666","#73C0DE","#3CA272","#FC8452","#9A60B4","#ea7ccc"],
-    dataLabel: false,
-    dataPointShape: false,
-    legend: {
-      show: false
-    },
-    xAxis: {
-      disableGrid: true,
-      boundaryGap: "justify",
-      format: "xAxisDemo2",
-      fontColor: '#92969A',
-      axisLine: false,
-      labelCount: 5
-    },
-    yAxis: {
-      gridType: "dash",
-      dashLength: 2,
-      data: [
-        {
-          axisLine: false,
-          fontColor: '#92969A',
-          min: 40,
-          max: 140
-        }
-      ]
-    },
-    extra: {
-      area: {
-        type: "curve",
-        opacity: 0.2,
-        addLine: true,
-        width: 2,
-        gradient: true,
-        activeType: "hollow"
-      },
-      tooltip: {
-        showBox: false,
-        splitLine:true,
-        gridColor: '#FDDEDE'
-      },
-    
-    }
-  }
-};
+  {
+    title: '平均静息心率',
+    value: '68'
+  },
+  {
+    title: '心率过高',
+    value: '--'
+  },
+  {
+    title: '心率过低',
+    value: '--'
+  },
+]
+
 
 </script>
 
 <style scoped lang="less">
 .hr-detail {
   padding: 16px 18px;
-  .hr-chart {
-    border: 2rpx solid #E9EEF0;
-    padding: 32rpx 10rpx 20rpx 0;
-    border-radius: 20rpx;
-    .hr-chart-title {
-      color: #92969A;
-      font-size: 20rpx;
-      line-height: 44rpx; 
-      text-align: center;
-      margin-bottom: 16rpx;
-      text {
-        color:rgba(0, 29, 69, 0.90);
-        font-size: 60rpx;
-        font-weight: 700;
-        line-height: 72rpx; 
-        margin-right: 12rpx;
+  .hr-card {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 32rpx;
+    .hr-card-item {
+      flex-basis: calc(50% - 68rpx); 
+      border: 2rpx solid #E9EEF0;
+      margin-bottom: 30rpx;
+      border-radius: 20rpx;
+      padding: 24rpx;
+      &:first-child {
+        background: #FF6C71;
+        color: white;
+      }
+      &:nth-child(odd) {
+        margin-right: 30rpx;
+      }
+      .title {
+        font-size: 24rpx;
+        line-height: 24rpx; 
+        margin-bottom: 20rpx;
+      }
+      .value {
+        font-size: 20rpx;
+        line-height: 24rpx;
+        text {
+          font-size: 40rpx;
+          font-weight: 700;
+          line-height: 52rpx;
+          margin-right: 8rpx;
+        }
       }
     }
   }
